72844a3b993761b421de4e5f044a34460b0d0a0f
Ben Moretti <benmoretti@icloud.com>
2019-02-27 19:59:28 +1030

    additional comments

--- 2019-02-26/french_trains_gganimate.R
+++ 2019-02-26/french_trains_gganimate.R
@@ -1,8 +1,12 @@
-#' french_trains.R
+#' french_trains_gganimate.R
 #'
 #' @author Ben Moretti
 #'
-#' @description Reads data for the 26 Feb 2019 #tidytuesday - French Trains - and visualises using ggalluvial
+#' @description 
+#' 
+#' Reads data for the 26 Feb 2019 #tidytuesday - French Trains - and visualises using ggalluvial
+#' Also attempts to add a gganimate animation for each month of the year 2017
+#' This does NOT work!
 
 
 # Libraries -----------------------------------------------------------------
@@ -19,7 +23,8 @@ trains_raw <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience
 
 # Condition ---------------------------------------------------------------
 
-# select data for paris departures for july 2017
+# select data for paris departures for 2017
+# we require the month so as to be able to loop over it in ggaminate
 paris_2017_trips <- trains_raw %>% 
   filter(
     year == 2017,
@@ -32,25 +37,37 @@ is_alluvia_form(paris_2017_trips, axes = 1:2, silent = TRUE)
 
 # Plot --------------------------------------------------------------------
 
-# create alluvial plot
+# create alluvial plot as per the original
 paris_2017_trips %>%
+  
+  # axis1 is on the left and axis2 on the right of the alluvial plot
   ggplot(aes(y = total_num_trips, axis1 = departure_station, axis2 = arrival_station)) +
+  
+  # this sets the fill colour to be the departure station
   geom_alluvium(aes(fill = departure_station)) +
   guides(fill = FALSE) +
+  
+  # this labels the "strata" of the alluvial plot with teh names of the stations
   geom_label(stat = "stratum",
              label.strata = TRUE,
              size = 2) +
+  
+  # for the x axis 
   scale_x_discrete(limits = c("Departure", "Arrival"),
                    expand = c(0.05, 0.05)) +
-  scale_fill_viridis_d() +
-  theme_minimal() +
-  theme(legend.position = "none") +
+  
+  # nice colours and minimal theme with no legend
+  scale_fill_viridis_d() + theme_minimal() + theme(legend.position = "none") +
+  
+  # some labels
   labs(
     title = "Train trips departing from Paris stations in July 2017",
     y = "Total Number of Trips",
     subtitle = "Source: SNCF",
     caption = "#tidytuesday by @benmoretti"
   ) +
+  
+  # finally call gganimate::transition_time to iterate over the month -- doesn't work though
   transition_time(month)
 
 # the final call to transition_time(month) throws the following error
@@ -58,8 +75,3 @@ paris_2017_trips %>%
 # Error in mapply(FUN = f, ..., SIMPLIFY = FALSE) : 
 # zero-length inputs cannot be mixed with those of non-zero length
 
-# Output ------------------------------------------------------------------
-
-
-#save png
-ggsave("2019-02-26/paris_july_2017_trains.png", units = "mm", width=297, height=210)
